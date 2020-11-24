cb293d6f42f5617bdfcfc7032a42cdf47929a328
Rodevp <rodriguezrodrigor12@gmail.com>
2020-11-24 09:23:26 -0500

    agregando estructura del proyecto

--- countrie/src/components/Header.vue
+++ countrie/src/components/Header.vue
@@ -1,17 +1,13 @@
 <template>
-  <div>
-  </div>
+
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  components: {
-  }
+  name: 'Header',
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
